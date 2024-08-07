7cf74b846749965592ef6f53f7ef6305798c29d8
Sunny Prakash <sunny.prakashgm@gmail.com>
2024-08-07 22:08:18 +0200

    fix middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 const admin = require("./firebase.config");
 class Middleware {
   async decodeToken(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
     try {
+      const token = req.headers.authorization.split(" ")[1];
       const decodeValue = await admin.auth().verifyIdToken(token);
       if (decodeValue) {
         req.user = decodeValue;
