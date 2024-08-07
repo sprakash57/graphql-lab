9445f6e0a3c63eb596df221d0917461ae0c7570f
Sunny Prakash <sunny.prakashgm@gmail.com>
2024-08-07 21:26:02 +0200

    setup firebase

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const middleware = require("./middleware");
+
+const app = express();
+const port = 5002;
+
+app.use(cors());
+
+app.use(middleware.decodeToken);
+
+app.get("/api/health", (req, res) => {
+  return res.send("OK");
+});
+
+app.get("/api/profile", (req, res) => {
+  console.log("api calling...");
+  return res.json({ email: req.user.email, userId: req.user.user_id });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
--- src/resolvers/User.js
+++
@@ -1,6 +0,0 @@
-const User = {
-    posts: (parent, args, { db }, info) => db.posts.filter(p => p.author === parent.id),
-    comments: (parent, args, { db }, info) => db.comments.filter(c => c.author === parent.id)
-}
-
-export default User;
